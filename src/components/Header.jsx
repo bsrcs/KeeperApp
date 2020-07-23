64e37c92d15073ba63f6ed0c51074d7d01ed93e5
bsrcs <bsr.ercelik@gmail.com>
2020-07-23 15:02:15 +0300

    Initial commit: Simple app with multiple React components

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Note from "./Note";
+
+
+function Header(){
+    return(
+    <header>
+        <title>Keeper App</title>
+        <h1>Keeper</h1>
+    </header>
+    );
+}
+
+export default Header;
